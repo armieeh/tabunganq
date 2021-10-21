f579c28094ddc3f2436a4724d5ad92726675aa7b
armieeh <armisaniasinarya@gmail.com>
2021-10-21 07:16:59 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<?php include 'sidebar.php' ?>
+</head>
+<body>
+    <?php include 'animasi.php' ?>
+</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+<nav class="navbar navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">TabunganQ</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title text-light" id="offcanvasNavbarLabel">TabunganQ</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+          <li class="nav-item">
+            <a class="bx bx-home mt-5 mb-5 fs-5 text-light" aria-current="page" href="#" style="text-decoration: none;">   Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="bx bx-dollar mt-2 mb-5 fs-5 text-light" href="#" style="text-decoration: none;">   Saldo</a>
+          </li>
+           <li class="nav-item">
+            <a class="bx bx-bar-chart-alt 2 mt-2 mb-5 fs-5 text-light" href="#" style="text-decoration: none;">   Riwayat</a>
+          </li>
+          <li class="nav-item">
+            <a class="bx bx-user mt-2 mb-5 fs-5 text-light" href="#" style="text-decoration: none;">   Akun</a>
+          </li>
+          <li class="nav-item">
+            <a class="bx bx-cog mt-2 mb-5 fs-5 text-light" href="#" style="text-decoration: none;">  Setting</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
